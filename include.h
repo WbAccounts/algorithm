013874e24dd2480002a3095285132cd501c449c9
wbaccounts <wbaccounts@163.com>
2024-11-12 17:42:15 +0800

    :sparkles: feat(main): 提交.gitignore和makefile
    Signed-off-by: wbaccounts <wbaccounts@163.com>

---
+++ include.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
